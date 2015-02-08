9ca1ae51ea5b8a13cc39217d2769a256c931a96e
Alex <alexgbahm@gmail.com>
2015-02-08 01:53:48 -0800

    Added a few links for left shark and made so it choses a random one and sends it. Also the 'Go Cougs!' command sends the wsu logo

--- handler/handler.go
+++ handler/handler.go
@@ -11,9 +11,17 @@ import(
     "fmt"
     "TwitterShell/twilio"
     "strings"
+    "math/rand"
 )
 
-var no = [11]string {"curl", "wget", "rm", "passwd", "chroot", "apt-get", "dpkg", "rmp", "sudo", "su", "chmod"}
+var no = [12]string {"curl", "wget", "rm", "passwd", "chroot", "apt-get", "dpkg", "rmp", "sudo", "su", "chmod", "hexdump"}
+var leftsharks = [6]string {
+	"http://pbs.twimg.com/media/B80Q0_3CIAAWy90.jpg",
+	"http://rack.0.mshcdn.com/media/ZgkyMDE1LzAyLzAzLzk4L2xlZnRzaGFya2luLjdhMWJjLmpwZwpwCXRodW1iCTk1MHg1MzQjCmUJanBn/50c64a3c/04e/leftsharkindiegogo.jpg",
+	"http://cdn.baeblemusic.com/images/bblog/sharkone.jpg",
+	"http://3dprint.com/wp-content/uploads/2015/02/leftsharkfeatured.jpg",
+	"https://cdn1.lockerdome.com/uploads/3543c4041c5326700a535f1257bacd8b29d516f0bebcd7e2bfedbe4097b5b3dd_large",
+	"http://media.giphy.com/media/yoJC2ESKKQZ5swxztS/giphy.gif"}
 
 //Normally blocking sterlization main function
 func Sterlhand(fromTwilio <-chan Twilio.TwilData, toProcess chan<- Twilio.TwilData, toTwilio chan<-Twilio.TwilData) {
@@ -22,9 +30,10 @@ func Sterlhand(fromTwilio <-chan Twilio.TwilData, toProcess chan<- Twilio.TwilDa
 	    cleanedMessage := toOperate.InMessage
 	    if !cleanMessage(cleanedMessage) || checkSpecial(cleanedMessage) {
 	        if strings.Contains("Go Cougs!", cleanedMessage) {
-	            toOperate.MediaURL = "http://pbs.twimg.com/media/B80Q0_3CIAAWy90.jpg" //sets shark
+	            toOperate.MediaURL = "http://hackathon.eecs.wsu.edu/images/cougar_logo.png" //sets hackathon image
 	        } else {
-	            toOperate.MediaURL = "http://pbs.twimg.com/media/B80Q0_3CIAAWy90.jpg"
+	        	i := rand.Int() % len(leftsharks)
+	        	toOperate.MediaURL = leftsharks[i]
 	        }
 	        fmt.Printf("toTwilio <- %v \n", toOperate)
 	        toTwilio <- toOperate //unsafe message, return to send back, no chance of running command
@@ -50,7 +59,7 @@ func cleanMessage(message string) bool{
 
 func checkSpecial(message string) bool {
     //check for "Go Cougs!" and "Left Shark"
-    if strings.Contains("Go Cougs!", message) || strings.Contains("Left Shark", message) {
+    if strings.Contains("Go Cougs!", message) || strings.Contains("LShark", message) {
         return true
     }
     return false
